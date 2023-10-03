718d9f40418b9e999c7a507f90bcfe56ccd64fa1
Yaokun <yiukuenchu@gmail.com>
2023-10-03 23:06:03 +1030

    Add tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  );
+export default function App() { 
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    )
+    
 }
-
-export default App;
