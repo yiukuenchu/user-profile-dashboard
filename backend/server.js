1b356d292c4b731f5c2a581c74e5e278d22b9cb6
Yaokun <yiukuenchu@gmail.com>
2023-10-07 16:01:15 +1030

    make it better

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import morgan from 'morgan';
 import connect from './database/conn.js';
 import router from './router/route.js';
+import ENV from './config.js';
+import * as path from 'path';
 
 const app = express();
 
@@ -14,13 +16,23 @@ app.disable('x-powered-by');
 
 const port = 8080;
 
-/** HTTP GET request */
-app.get('/', (req, res) => {
-  res.status(201).json('Home GET Request');
-});
-
 /** API routes */
 app.use('/api', router);
+
+/** ---------------Deployment---------------- */
+const __dirname1 = path.resolve();
+if (ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname1, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  /** HTTP GET request */
+  app.get('/', (req, res) => {
+    res.status(201).json('Home GET Request');
+  });
+}
 
 /** start server only when connected to database*/
 connect()
